632211adafdfdbfee35973076d51f5b8beac8011
Joshabe1234 <147118149+Joshabe1234@users.noreply.github.com>
2026-02-08 15:13:24 -0600

    Update vite.config.js
    new codebase

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  server: {
-    historyApiFallback: true,
-  },
-   build: {
-    outDir: 'dist',
-    sourcemap: true
-  },
-});
+})
